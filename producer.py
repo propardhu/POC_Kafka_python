7bcc7b32c8f4fcf8be7bc71a69cf44bd8705ab3a
Parthasai <53685926+propardhu@users.noreply.github.com>
2024-02-17 14:49:26 -0600

    Update producer.py

--- producer.py
+++ producer.py
@@ -5,12 +5,14 @@ import time
 producer = KafkaProducer(bootstrap_servers='localhost:9092')
 
 def produce_messages():
-    for _ in range(100):
-        message = "This is a test message".encode('utf-8')
+    count = 1
+    for _ in range(1000):
+        message = "This is a test message number {count}".encode('utf-8')
+        count = count +1
         producer.send('test_topic', message)
-        time.sleep(0.1) # Sleep for 0.1 seconds to send 10 messages per second
+        time.sleep(0.1)
 
 if __name__ == "__main__":
     produce_messages()
-    producer.flush() # Ensure all messages are sent before closing
+    producer.flush()
     producer.close()
